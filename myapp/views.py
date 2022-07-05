98da08a95e9cdbe783ba082909a4d9271b982c9e
minji Lim <isk09137@naver.com>
2022-07-05 12:09:01 +0900

    first commit

--- myapp/views.py
+++ myapp/views.py
@@ -1,3 +1,25 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from .forms import BlogForm
+from django.utils import timezone
+from .models import Blog
 
 # Create your views here.
+
+def main(request):
+    return render(request, 'main.html')
+
+def write(request):
+    if request.method == 'POST':
+        form = BlogForm(request.POST)
+        if form.is_valid():
+            form = form.save(commit=False)
+            form.pub_date = timezone.now()
+            form.save()
+            return redirect('main')
+    else:
+        form = BlogForm
+        return render(request, 'write.html', {'form':form})
+
+def read(request):
+    blogs = Blog.objects
+    return render(request, 'read.html', {'blogs':blogs})
--- myproject/urls.py
+++ myproject/urls.py
@@ -15,7 +15,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+import myapp.views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', myapp.views.main, name='main'),
+    path('write/', myapp.views.write, name='write'),
+    path('read/', myapp.views.read, name='read'),
 ]
